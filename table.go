2ebb7d2b365db918e76e6923da3efdc16f889527
xiejianjun <xiejianjun@bytedance.com>
2023-02-16 19:40:52 +0800

    add PrintTableRich

--- table.go
+++ table.go
@@ -17,3 +17,29 @@ func PrintTable(header []string, data [][]string) {
 	table.AppendBulk(data)
 	table.Render()
 }
+
+type ColorFunc func(row []string) []tablewriter.Colors
+
+func PrintTableRich(header []string, data [][]string, mergeCols []int, colorFunc ColorFunc) {
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetAutoFormatHeaders(false)
+	table.SetAutoWrapText(true)
+	table.SetReflowDuringAutoWrap(false)
+	table.SetColWidth(120)
+	table.SetBorder(true)
+	table.SetRowLine(true)
+	if mergeCols != nil {
+		table.SetAutoMergeCellsByColumnIndex(mergeCols)
+	}
+	if len(header) > 0 {
+		table.SetHeader(header)
+	}
+	if colorFunc == nil {
+		table.AppendBulk(data)
+	} else {
+		for _, row := range data {
+			table.Rich(row, colorFunc(row))
+		}
+	}
+	table.Render()
+}
